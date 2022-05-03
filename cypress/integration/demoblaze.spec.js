507760c6590bab9bd66b22fe9d6fa3a2279bb3f2
José <joseneto.2762@aluno.una.br>
2022-05-03 01:31:44 -0300

    meia refatoração para melhora e adição new func

--- cypress/integration/demoblaze.spec.js
+++ cypress/integration/demoblaze.spec.js
@@ -1,32 +1,30 @@
-
 describe('Teste do site demoblaze', () => {
-    beforeEach(function(){
-        cy.visit('http://www.demoblaze.com/')
-    })
-    it('Testar seletores CSS', () => {
-     
-        cy.get(`a.nav-link[href="index.html"]`)
-        .should('have.attr', 'href', 'index.html').should('include.text','Home')
-        cy.contains('a','Contact').click()
-        cy.wait(2000)
-        cy.get('#exampleModal > .modal-dialog > .modal-content > .modal-footer > .btn-secondary')
-        .click()
-        cy.get(`a[data-target="#exampleModal"]`)
-        .should('have.attr', 'href')
-        cy.get(`a[data-target="#videoModal"]`)
-        .should('have.attr', 'href')
-        cy.get(`[onclick="byCat('phone')"]`).should('be.visible')    
-})
-    it('Testar About Us', () => {
-        cy.get(`a[data-target="#videoModal"]`).click()
-        cy.wait(2000)
-        cy.get('#videoModal > .modal-dialog > .modal-content > .modal-footer > .btn')
-        .click()
-        cy.get(`a[data-target="#videoModal"]`).click()
-        cy.wait(2000)
-        cy.get('#videoModal > .modal-dialog > .modal-content > .modal-header > .close > span')
-        .click()
-        
-    });
-    
+	beforeEach(function () {
+		cy.visit('http://www.demoblaze.com/')
+	})
+	it('Testar seletores CSS', () => {
+		cy.get(`a.nav-link[href="index.html"]`)
+			.should('have.attr', 'href', 'index.html')
+			.should('include.text', 'Home')
+		cy.contains('a', 'Contact').click()
+		cy.wait(2000)
+		cy.get(
+			'#exampleModal > .modal-dialog > .modal-content > .modal-footer > .btn-secondary'
+		).click()
+		cy.get(`a[data-target="#exampleModal"]`).should('have.attr', 'href')
+		cy.get(`a[data-target="#videoModal"]`).should('have.attr', 'href')
+		cy.get(`[onclick="byCat('phone')"]`).should('be.visible')
+	})
+	it('Testar About Us', () => {
+		cy.get(`a[data-target="#videoModal"]`).click()
+		cy.wait(2000)
+		cy.get(
+			'#videoModal > .modal-dialog > .modal-content > .modal-footer > .btn'
+		).click()
+		cy.get(`a[data-target="#videoModal"]`).click()
+		cy.wait(2000)
+		cy.get(
+			'#videoModal > .modal-dialog > .modal-content > .modal-header > .close > span'
+		).click()
+	})
 })
